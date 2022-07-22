4fb718c5151957b161b4d20e27673c3f4963903c
Ali Bagheri <khodealib@gmail.com>
2022-07-22 16:33:03 +0430

    create blog app.

---
+++ src/blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from blog.views import *
+
+app_name = 'blog'
+urlpatterns = [
+]
---
+++ src/blog/views.py
@@ -0,0 +1,3 @@
+from http.client import HTTPResponse
+from django.http import HttpResponse
+from django.shortcuts import render
