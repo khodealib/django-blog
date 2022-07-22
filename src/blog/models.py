26c24c22030c33543fe6b59136c53de12fb86365
Ali Bagheri <khodealib@gmail.com>
2022-07-22 16:52:47 +0430

    Add Article model.

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,1 +1,19 @@
+from time import timezone
+from venv import create
 from django.db import models
+from django.utils import timezone
+
+
+class Article(models.Model):
+    STATUS = (
+        ("d", "Draft"),
+        ("p", "Published")
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100, unique=True)
+    descriptions = models.TextField()
+    thumbnail = models.ImageField(upload_to="images")
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, choices=STATUS)
+    
