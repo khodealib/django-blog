bce0930b438ca5cdc79b5209257daa58aa753ab2
Ali Bagheri <khodealib@gmail.com>
2022-07-24 21:02:25 +0430

    create ArticleAdmin class.

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Article as ArticleModel
 
-admin.site.register(ArticleModel)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'publish', 'status')
+    list_filter = ('publish', 'status')
+    search_fields = ('title', 'descriptions')
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ['-status', '-publish']
+
+
+admin.site.register(ArticleModel, ArticleAdmin)
