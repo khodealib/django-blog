41b4ba2a9728b865ddd77002e218c5fceaaddd46
Ali Bagheri <khodealib@gmail.com>
2022-09-10 19:59:32 +0430

    Add blog urls and media, Implement home and detail views

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,21 @@
-from http.client import HTTPResponse
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from blog.models import Article
+
+
+def home(request):
+    articles = Article.objects.filter(status='p').order_by('-publish')
+    template = 'blog/home.html'
+    context = {
+        "articles": articles
+    }
+    return render(request, template, context)
+
+
+def detail(request, slug):
+    article = get_object_or_404(Article, slug=slug)
+    template = 'blog/detail.html'
+    context = {
+        "article": article
+    }
+    return render(request, template, context)
--- src/config/urls.py
+++ src/config/urls.py
@@ -1,14 +1,16 @@
-from . import settings as project_settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path
+
+from . import settings as project_settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 if project_settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(project_settings.MEDIA_URL, document_root=project_settings.MEDIA_ROOT)
+else:
+    urlpatterns += staticfiles_urlpatterns()
